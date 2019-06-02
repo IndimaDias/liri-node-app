70c03bda093b0ec1973a533e1672b4f77cfde7f0
IndimaDias <indimad@yahoo.com>
2019-06-02 18:51:14 -0400

    Version 1.3 Display Band events -Read results and display band details in the terminal

--- liri.js
+++ liri.js
@@ -7,7 +7,13 @@ var keys = require("./keys.js");
 // Include the axios npm package
 var axios = require('axios');
 
+// Include node-spotify-api package
 var Spotify = require("node-spotify-api");
+
+// Include moment package
+
+var moment = require("moment");
+// moment().format();
 
 // access API keys
 var spotify = new Spotify(keys.spotify);
@@ -28,7 +34,7 @@ console.log(userCommand + " " + request);
 
 switch(userCommand){
     case "concert-this":
-        queryURL = "https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp";
+        queryURL = "https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp?limit=5";
         break;
     
     case "movie-this":
@@ -37,12 +43,24 @@ switch(userCommand){
 
 
 }
-
+var queryResults = [];
 // run axios request to the api 
 axios.get(queryURL).then(
     function(response){
-        console.log(response.data);
+    //    assign data to an array 
+        queryResults = response.data;
+        // console.log(queryResults);
+        // read the array 
+        for(var i =0; i,queryResults.length; i++){
+            console.log("Venue name : " + queryResults[i].venue.name + 
+                        "\nVenue Location : " + queryResults[i].venue.city + " " + queryResults[i].venue.country +
+                        "\nDate of the event : " + moment(queryResults[i].datetime).format("MM/DD/YYYY"));
+                        console.log("                                             ");
+                        console.log("****************************************************************");
+                        console.log("                                             ");
+        }
     }
 ).catch(function(error){
     error.message;
 })
+
