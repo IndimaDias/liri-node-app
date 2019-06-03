74f5b56796cff939a7cb43959ffb5bb93ab2fd75
IndimaDias <indimad@yahoo.com>
2019-06-02 21:57:04 -0400

    Version 2.0 - Modified to read results and display movie detials

--- liri.js
+++ liri.js
@@ -25,24 +25,33 @@ var args = process.argv;
 var userCommand = args[2];
 
 // reading the user search request (song name , movie name , band name);
-var request = args[3];
+// %20 code for white space is assigned if user enters a name with spaces 
+var request = args.slice(3,args.length).join("%20");
 
 // variable to assign the url 
 var queryURL = "";
 
-console.log(userCommand + " " + request);
+// console.log(userCommand + " " + request);
 
+// validate user command and create url for the API
 switch(userCommand){
     case "concert-this":
         queryURL = "https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp?limit=5";
         break;
     
     case "movie-this":
+        // if user didn't enter a movie name
+        if (request === null) {
+            request = "'Mr. Nobody."
+        }
         queryURL =  "http://www.omdbapi.com/?t=" + request + "&y=&plot=short&apikey=trilogy";
+        
         break;
 
 
 }
+
+
 var queryResults = [];
 // run axios request to the api 
 axios.get(queryURL).then(
@@ -50,14 +59,42 @@ axios.get(queryURL).then(
     //    assign data to an array 
         queryResults = response.data;
         // console.log(queryResults);
-        // read the array 
-        for(var i =0; i,queryResults.length; i++){
-            console.log("Venue name : " + queryResults[i].venue.name + 
-                        "\nVenue Location : " + queryResults[i].venue.city + " " + queryResults[i].venue.country +
-                        "\nDate of the event : " + moment(queryResults[i].datetime).format("MM/DD/YYYY"));
-                        console.log("                                             ");
-                        console.log("****************************************************************");
-                        console.log("                                             ");
+
+        
+        if(userCommand === 'movie-this'){
+            // find Rotten tomatoes rating for the movie
+            try{
+                // find object with source Rotten Tomatoes in the array of Ratings and get the rating value
+                var rating = queryResults.Ratings.find(ratingObj => ratingObj.Source === "Rotten Tomatoes").Value;          
+            }catch(err){
+                // if the Rotten tomato rating is not defined 
+               rating = "Rating not available";
+            }
+                        
+            console.log("Title : " + queryResults.Title + 
+            "\nYear : " + queryResults.Year +
+            "\nIMDB Rating : " + queryResults.imdbRating +
+            "\nRotten Tomatoes Ratin : " + rating +
+            "\nCountry : " + queryResults.Country + 
+            "\nLanguage : " + queryResults.Language +
+            "\nPlot : " + queryResults.Plot +
+            "\nActors : " + queryResults.Actors);
+
+        }
+        else if (userCommand === 'concert-this'){
+            // read the array in a for loop and display results. 
+            // moment package is used to format the event date
+            for(var i =0; i,queryResults.length; i++){
+
+                console.log("Venue name : " + queryResults[i].venue.name + 
+                "\nVenue Location : " + queryResults[i].venue.city + " " + queryResults[i].venue.country +
+                "\nDate of the event : " + moment(queryResults[i].datetime).format("MM/DD/YYYY"));                
+            }
+
+            console.log("                                             ");
+            console.log("****************************************************************");
+            console.log("                                             ");
+
         }
     }
 ).catch(function(error){
